b376cfb19a69ebae2ffde3c331bd226856122caf
Sesha997 <seshasaisingaraju@hotmail.com>
2019-07-31 04:46:16 +0530

    Jobs perfect. Need to update blogs

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=50)
+    pubdate = models.DateTimeField()
+    body = models.CharField(max_length=500)
+    image = models.ImageField(upload_to='images/')
