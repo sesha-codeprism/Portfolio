f825fda68e36324cd73f21af7fb409f9266b1276
Sesha997 <seshasaisingaraju@hotmail.com>
2019-08-04 01:27:08 +0530

    One perfect application

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,12 @@ class Blog(models.Model):
     pubdate = models.DateTimeField()
     body = models.CharField(max_length=500)
     image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:50]
+
+    def publ_date(self):
+        return self.pubdate.strftime('%b %e %Y')
